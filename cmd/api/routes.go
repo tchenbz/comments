b622dcdfeae964ee03fc123d6bf1a4ae52fe9962
Tamika Chen <2019120211@ub.edu.bz>
2024-10-02 13:11:33 -0600

    encode version 7

---
+++ cmd/api/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	//"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (a *applicationDependencies)routes() http.Handler {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(a.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedResponse)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthCheckHandler) 
+	return router
+}
