9f7ef36b4f236f52ffd60536426cf91093403d8b
Tamika Chen <2019120211@ub.edu.bz>
2024-10-14 09:52:39 -0600

    connection pool setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+	_ "github.com/lib/pq"
 )
 
 const appVersion = "1.0.0"
@@ -14,6 +17,9 @@ const appVersion = "1.0.0"
 type serverConfig struct {
 	port int
 	environment string
+	db struct {
+        dsn string
+    }
 }
 
 type applicationDependencies struct {
@@ -26,9 +32,23 @@ func main() {
 
 	flag.IntVar(&settings.port, "port", 4000, "Server port")
 	flag.StringVar(&settings.environment, "env", "development", "Environment(development|staging|production)")
+	flag.StringVar(&settings.db.dsn, "db-dsn", "postgres://comments:fishsticks@localhost/comments?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// the call to openDB() sets up our connection pool
+	db, err := openDB(settings)
+	if err != nil {
+    logger.Error(err.Error())
+    os.Exit(1)
+	}
+
+	// release the database resources before exiting
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+
 
 	appInstance := &applicationDependencies {
 		config: settings,
@@ -48,7 +68,27 @@ func main() {
 	}
 
 	logger.Info("starting server", "address", apiServer.Addr, "environment", settings.environment)
-	err := apiServer.ListenAndServe()
+	err = apiServer.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(settings serverConfig) (*sql.DB, error) {
+    // open a connection pool
+    db, err := sql.Open("postgres", settings.db.dsn)
+    if err != nil {
+        return nil, err
+    }
+    
+    // set a context to ensure DB operations don't take too long
+    ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+    defer cancel()
+	err = db.PingContext(ctx)
+    if err != nil {
+        db.Close()
+        return nil, err
+    }
+
+    // return the connection pool (sql.DB)
+    return db, nil
 }
